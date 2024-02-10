5687b7930dad2b5f074c6ff7afdbe82bab0a5280
Abc Def <abc@Abcs-iMac-Pro.local>
2024-02-10 10:56:22 -0800

    a

--- AS-Scramble/AS-Scramble/Constants.swift
+++ AS-Scramble/AS-Scramble/Constants.swift
@@ -5,10 +5,10 @@
 //  Created by user248197 on 2/7/24.
 //
 
-//import SpriteKit
+import Foundation
 
 class Constants {
     public static let SCENE_WIDTH = 1024
     public static let NB_MOUNTAINS = 10
-    public static let MOUNTAIN_WIDTH = Float(SCENE_WIDTH / NB_MOUNTAINS + 30)
+    public static let MOUNTAIN_WIDTH = CGFloat(SCENE_WIDTH / NB_MOUNTAINS + 30)
 }
