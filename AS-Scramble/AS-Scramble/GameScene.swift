c81a4a6f75a1bb26dff323a7a5355a6e88e3a01c
Abc Def <abc@Abcs-iMac-Pro.local>
2024-02-10 12:11:20 -0800

    a

--- AS-Scramble/AS-Scramble/GameScene.swift
+++ AS-Scramble/AS-Scramble/GameScene.swift
@@ -68,6 +68,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func didMove(to view: SKView) {
+        self.addChild(Bullet())
+        
         //self.scaleMode = .resizeFill
         
         scoreLabel.text = "Score: \(self.score)"
