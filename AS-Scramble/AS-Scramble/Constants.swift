ead6128e7586f2fc89faea38bb5a4db0f0e0bd4d
Abc Def <abc@Abcs-iMac-Pro.local>
2024-02-10 11:00:38 -0800

    a

--- AS-Scramble/AS-Scramble/Constants.swift
+++ AS-Scramble/AS-Scramble/Constants.swift
@@ -9,6 +9,7 @@ import Foundation
 
 class Constants {
     public static let SCENE_WIDTH = 1024
-    public static let NB_MOUNTAINS = 10
-    public static let MOUNTAIN_WIDTH = CGFloat(SCENE_WIDTH / NB_MOUNTAINS + 30)
+    public static let NB_MOUNTAINS_ON_SCREEN = 10
+    public static let REAL_NB_MOUNTAINS = NB_MOUNTAINS_ON_SCREEN + 2
+    public static let MOUNTAIN_WIDTH = CGFloat(SCENE_WIDTH / NB_MOUNTAINS_ON_SCREEN + 30)
 }
